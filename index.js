d4b900c3529eba495d537c053f5d12e0c82f93c7
Berkay Binici <berkay133181@gmail.com>
2026-02-20 01:44:25 +0300

    Add Swagger OpenAPI documentation

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 import express from "express";
 import pinoHttp from "pino-http";
+import swaggerUi from "swagger-ui-express";
+import fs from "fs";
+import path from "path";
+import YAML from "yaml";
+
 import { taskRoutes } from "./src/api/routes/taskRoutes.js";
 import { authRoutes } from "./src/api/routes/authRoutes.js";
 import { logger } from "./src/infrastructure/logger/logger.js";
@@ -8,11 +13,20 @@ import { env } from "./src/config/env.js";
 
 const app = express();
 
+/* ---------- MIDDLEWARES ---------- */
 app.use(express.json());
 app.use(pinoHttp({ logger }));
 
+/* ---------- SWAGGER ---------- */
+const openapiPath = path.resolve("src/docs/openapi.yaml");
+const openapiYaml = fs.readFileSync(openapiPath, "utf8");
+const openapiDoc = YAML.parse(openapiYaml);
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(openapiDoc));
+
+/* ---------- ROUTES ---------- */
 app.get("/", (req, res) => {
-  res.send("Task Service is running. Try /health, /auth/login and /tasks");
+  res.send("Task Service is running. Try /health, /docs, /tasks");
 });
 
 app.get("/health", (req, res) => {
@@ -22,8 +36,10 @@ app.get("/health", (req, res) => {
 app.use("/auth", authRoutes);
 app.use("/tasks", taskRoutes);
 
+/* ---------- ERROR HANDLER ---------- */
 app.use(errorHandler);
 
+/* ---------- START ---------- */
 app.listen(env.PORT, () => {
-  console.log(`Task Service running on http://localhost:${env.PORT}`);
+  console.log(`🚀 Server running on http://localhost:${env.PORT}`);
 });
